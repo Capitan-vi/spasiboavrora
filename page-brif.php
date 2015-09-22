b44bbed85b2f796b7705c0ea018518d31ed8ef66
Valentin Emelyanov <valentin.html@gmail.com>
2015-09-22 22:31:23 +0300

    added front-slider to brif page

--- page-brif.php
+++ page-brif.php
@@ -9,14 +9,20 @@ get_header(); ?>
 
 
   <div class="this_page">
-  <div class="row row-fluid">
-    <div class="page-content">
-      <?php the_post(); ?>
-      <?php the_content(); ?>
 
-      <?php echo do_shortcode('[contact-form-7 id="27" html_class="b-form-brif" title="Бриф"]'); ?>
+    <div class="row row-fluid">
+        <?php get_template_part('template-parts/front-slider'); ?>
     </div>
-  </div>
+
+    <div class="row row-fluid">
+      <div class="page-content">
+        <?php the_post(); ?>
+        <?php the_content(); ?>
+
+        <?php echo do_shortcode('[contact-form-7 id="27" html_class="b-form-brif" title="Бриф"]'); ?>
+      </div>
+    </div>
+
   </div>
 
 <?php get_footer(); ?>
