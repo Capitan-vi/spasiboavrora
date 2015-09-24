c4c510cbe1855f6a0cb134f4274cecfc21da599a
Valentin Emelyanov <valentin.html@gmail.com>
2015-09-24 14:49:00 +0300

    added custom field for front-slider

--- template-parts/front-slider.php
+++ template-parts/front-slider.php
@@ -1,6 +1,10 @@
 <div class="flexslider mb0" id="port_slider">
   <ul class="slides">
-    <li><img src="<?php bloginfo("template_directory"); ?>/assets/images/front-slider_slide1.jpg" alt="" draggable="false"></li>
-    <li><img src="<?php bloginfo("template_directory"); ?>/images/m1.jpg" alt="" draggable="false"></li>
+    <?php if( get_field('options__repeater-slider', 'options') ):
+      while( has_sub_field('options__repeater-slider', 'options') ): ?>
+
+        <li><img src="<?php the_sub_field('options__front-slider-img', 'options'); ?>" alt="" draggable="false"></li>
+
+    <?php endwhile; endif; ?>
   </ul>
 </div>
